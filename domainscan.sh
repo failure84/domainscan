cfce19626555996b65c62dff781cc1171adda8f2
Patrik Båt <pb@osix.eu>
2017-09-18 16:35:47 +0000

    fix wrong sql syntax in domainscan.sh

--- domainscan.sh
+++ domainscan.sh
@@ -18,7 +18,7 @@ mysql -u root -pPASSWORD -e "update domains_records as DR set DR.vendor_id = 1 w
 
 mysql -u root -pPASSWORD -e "update domains set vendor_id = IFNULL((select vendor_id from domains_records where domain_id = domains.id order by modified desc limit 1), 1) where id = domains.id;" domainscan
 
-mysql -u root -pPASSWORD -e "insert into stats ( id, date, vendor_id, total_domains) SELECT NULL, NOW(), Vendors.id, (     COUNT(Domains.id)   ) AS `total_domains`  FROM    vendors Vendors    INNER JOIN domains Domains ON Vendors.id = (Domains.vendor_id)  GROUP BY    Domains.vendor_id  ORDER BY    total_domains DESC;" domainscan
+mysql -u root -pPASSWORD -e "insert into stats ( id, date, vendor_id, total_domains) SELECT NULL, NOW(), Vendors.id, (     COUNT(Domains.id)   ) AS total_domains  FROM    vendors Vendors    INNER JOIN domains Domains ON Vendors.id = (Domains.vendor_id)  GROUP BY    Domains.vendor_id  ORDER BY    total_domains DESC;" domainscan
 
 #mysql -u root -pPASSWORD -e "delete from domains_records where created < DATE_ADD(DATE(NOW()), INTERVAL -7 DAY);" domainscan
 
